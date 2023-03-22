413f5edcc7558a027ebe749f166c78ba4fb84732
Aimee Esler <aimeecesler@gmail.com>
2023-03-22 18:15:14 -0400

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Aimee Esler on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
